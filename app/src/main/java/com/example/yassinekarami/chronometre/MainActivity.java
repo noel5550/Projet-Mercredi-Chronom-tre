bb8a0a16d1aaac8e776401733d8749571275cb37
yassinekarami <yassine.karami@outlook.com>
2018-10-05 18:51:57 +0000

    interface graphique

--- app/src/main/java/com/example/yassinekarami/chronometre/MainActivity.java
+++ app/src/main/java/com/example/yassinekarami/chronometre/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.yassinekarami.chronometre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timer;
+    Button btn_start;
+    Button btn_reset;
+    Button btn_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
