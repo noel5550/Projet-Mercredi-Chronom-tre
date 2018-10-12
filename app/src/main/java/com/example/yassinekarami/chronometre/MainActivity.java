c4d653ab8fab315d618c8721a3046844ee7eb6d4
yassinekarami <yassine.karami@outlook.com>
2018-10-12 23:47:19 +0000

    refonte

--- app/src/main/java/com/example/yassinekarami/chronometre/MainActivity.java
+++ app/src/main/java/com/example/yassinekarami/chronometre/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yassinekarami.chronometre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.Chronometer;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -9,19 +11,23 @@ import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView timer;
     private Button btn_start;
-    //private Button btn_reset;
+    private Button btn_stop;
+    private Button btn_reset;
     private Button btn_laps;
+
+
     private ScrollView scroll;
 
     private int lap;
     private TextView textLaps;
+    private long stop;
 
 
 
     private Chronometer chronometer;
-    private Thread threadChrono;
+
+
 
 
     @Override
@@ -29,22 +35,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timer = (TextView)findViewById(R.id.timer);
-       // btn_reset = (Button)findViewById(R.id.btn_reset);
+        chronometer = (Chronometer)findViewById(R.id.chrono);
+
         btn_start = (Button)findViewById(R.id.btn_start);
+
+        btn_stop = (Button)findViewById(R.id.btn_stop);
+        btn_reset = (Button)findViewById(R.id.btn_reset);
+
         btn_laps = (Button)findViewById(R.id.btn_laps);
         textLaps = (TextView)findViewById(R.id.laps_text);
+
         scroll = (ScrollView)findViewById(R.id.scroll);
 
-        textLaps.setEnabled(false);
+        btn_reset.setVisibility(View.GONE);
+    }
+
+    public void StartClick(View view)
+    {
+        if (stop != 0)
+        {
+            chronometer.setBase(chronometer.getBase() + SystemClock.elapsedRealtime() - stop);
+        }
+        else
+        {
+            chronometer.setBase(SystemClock.elapsedRealtime());
+        }
+        chronometer.start();
+        lap = 1;
+
+        btn_reset.setVisibility(View.GONE);
+        btn_stop.setVisibility(View.VISIBLE);
+
 
     }
+
+    public void StopClick(View view)
+    {
+        stop = SystemClock.elapsedRealtime();
+        chronometer.stop();
+        btn_reset.setVisibility(View.VISIBLE);
+        btn_stop.setVisibility(View.GONE);
+    }
+
+    public void ResetClick(View view)
+    {
+
+        textLaps.setText("\n");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        stop = 0;
+    }
+
 
     public void LapsClick(View view)
     {
         if (chronometer != null)
         {
-            textLaps.append("Laps " + String.valueOf(lap)+" : " +String.valueOf(timer.getText()) +" \n");
+            textLaps.append("Laps " + String.valueOf(lap)+" : " +String.valueOf(chronometer.getText()) +" \n");
             lap ++ ;
             scroll.post(new Runnable() {
                 @Override
@@ -56,52 +102,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void StartClick(View view)
-    {
-        if (chronometer == null)
-        {
-            chronometer = new Chronometer(this);
-            threadChrono = new Thread(chronometer);
-            threadChrono.start();
-            chronometer.Start();
-
-            lap = 1;
-            textLaps.setText("");
-
-        }
-    }
-
-    public void ResetClick(View view)
-    {
-        if (chronometer != null )
-        {
-            chronometer.Reset();
-            threadChrono.interrupt();
-            threadChrono = null;
-            chronometer = null;
-            btn_start.setText("Start");
-        }
-
-
-    }
-
-
-    public void StopClick(View view)
-    {
-        if (chronometer != null)
-        {
-            chronometer.Stop();
-        }
-    }
-
-
-    public void updateTimerText(final String time)
-    {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                timer.setText(time);
-            }
-        });
-    }
 }
